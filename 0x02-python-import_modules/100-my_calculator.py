6439ca86d28d66609039830d2b6d1d0fdb1258ea
“Dino” <daringamaleldin@hotmail.com>
2023-11-30 12:28:24 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,13 @@ if __name__ == "__main__":
     if dino != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-    if op == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op  == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif op == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+
+    op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+     print("{} {} {} = {}".format(a,sys.argv[2], b, op[sys.argv[2]](a, b)))
+
