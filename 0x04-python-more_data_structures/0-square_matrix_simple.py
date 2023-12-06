cc28a8d5ef9dd34266452e668766cd3da16abf86
“Dino” <daringamaleldin@hotmail.com>
2023-12-06 05:49:28 -0800

    square matrixe

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        return[[i**2 for x in line] for line in matrix]
+        return[[i**2 for x in row] for row in matrix]
