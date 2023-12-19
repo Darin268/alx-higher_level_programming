15aa856bf7167f649ffbe591d1f2e9b535887972
“Dino” <daringamaleldin@hotmail.com>
2023-12-19 11:24:22 -0800

    6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,6 +3,11 @@
 
 
 class Square:
+    """Class Square that defines a square
+
+    Attributes:
+    __size (int): size of a size of the square
+    __position (tuple): position of the square in 2D"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
