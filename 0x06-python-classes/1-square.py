2c0f9ec82c86080b2595b03cf5ed09fde2f1f76a
“Dino” <daringamaleldin@hotmail.com>
2023-12-19 09:25:33 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """A class square"""
 
+
 class Square:
-    """To define a square"""
+
 
     def __init__(self, size):
         """Private instance attribute: size
-    Instantiation with size (no type/value verification)
-    You are not allowed to import any module"""
+        Instantiation with size (no type/value verification)
+        You are not allowed to import any module"""
         self.__size = size
