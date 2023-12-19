ce58d6a39138f7ba78f13b9a366047244e8b249a
“Dino” <daringamaleldin@hotmail.com>
2023-12-19 09:32:52 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,9 @@
 
 class Square:
         """Private instance attribute: size
-        Instantiation with size (no type/value verification)"""
+
+        Instantiation with size (no type/value verification).
+
+        """
         def __init__(self, size):
             self.__size = size
