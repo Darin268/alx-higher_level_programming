fdb8f69991023e00595e3ba9f9580e202314fcd4
“Dino” <daringamaleldin@hotmail.com>
2024-02-14 13:39:42 -0800

    all

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- Lists all genres from `hbtn_0d_tvshows` and displays
 -- the number of shows linked to each.
-
 SELECT genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 JOIN genres ON tv_show_genres.genre_id = genres.id
