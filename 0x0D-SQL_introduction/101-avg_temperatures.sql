d6d7957c0866fa2c4fb6ec65e78f3b1e9304e0d4
“Dino” <daringamaleldin@hotmail.com>
2024-02-14 13:55:31 -0800

    task 101

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,15 @@
+-- Displays the average temperature (Fahrenheit) by city
+-- ordered by temperature (descending).
+
+USE hbtn_0c_0;
+
+SELECT
+    city,
+    AVG(temperature) AS avg_temp
+FROM
+    temperatures
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC;
+
