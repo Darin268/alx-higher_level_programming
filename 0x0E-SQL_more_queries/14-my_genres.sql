4e099d2a643cfe08c932e9b5843acde34c4271d1
“Dino” <daringamaleldin@hotmail.com>
2024-02-14 13:46:49 -0800

    all

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
--- Lists all genres of the
--- Dexter` show.
-
-SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- uses a databse to lists all rows in a table corresponding to all rows in another
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+GROUP BY name
+ORDER BY name ASC;
