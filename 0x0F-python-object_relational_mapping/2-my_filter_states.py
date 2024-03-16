b6bb07d68cf1bd9e04e7ae5c2572147ea8ebf82b
“Dino” <daringamaleldin@hotmail.com>
2024-03-16 13:03:42 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Database connection parameters
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute SQL query to select states matching the input state name
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all rows from the result set
-    rows = cursor.fetchall()
-
-    # Print results
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    # Close cursor and database connection
-    cursor.close()
+    cur.close()
     db.close()
