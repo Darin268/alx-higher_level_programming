f5aa039b1611f1bea36ab8febd8f9852bc21303c
“Dino” <daringamaleldin@hotmail.com>
2024-03-16 14:06:09 -0700

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-""" Deletes all State objects with a name containing the letter a
-    from the database hbtn_0e_6_usa
+""" prints the State object with the name passed as argument from the database
 """
-
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
-
-    # Create session maker bound to the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create session
     session = Session()
-
-states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states_to_delete:
-        session.delete(state)
-
-    # Commit changes
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
     session.commit()
-
-    # Close session
-    session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""Print all City objects from the database hbtn_0e_14_usa."""
-
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
-
-    # Create session maker bound to the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create session
     session = Session()
-
-    # Query all City objects
-    cities = session.query(City).order_by(City.id).all()
-
-    # Print results
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-
-    # Close session
-    session.close()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
