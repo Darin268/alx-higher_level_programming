80a3fc21a555f2d5250b3575d86700a89937e685
“Dino” <daringamaleldin@hotmail.com>
2024-03-27 16:51:38 -0700

    update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script sends a request to a URL passed as an argument and displays only the status code of the response
+# Sends a GET request to a given URL and display the response status code.
 status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
 echo "$status_code"
